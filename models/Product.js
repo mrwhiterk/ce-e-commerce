aa4c62ececb3de5d47da6715204826e3f2c1245c
Ryan White <mrwhiterk@gmail.com>
2019-11-04 14:06:51 -0500

    Yuri's change

--- models/Product.js
+++ models/Product.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose')
 const mongoosastic = require('mongoosastic')
 
+require('dotenv').config()
+
 const ProductSchema = new mongoose.Schema({
   category: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Category',
+    ref: 'category',
     es_type: 'nested',
     es_include_in_parent: true
   },
   name: { type: String, es_type: 'text', default: '' },
-  price: { type: Number, es_type: 'integer', default: 0 },
+  price: { type: Number, es_type: 'long', default: 0 },
   image: { type: String, es_type: 'text', default: '' }
 })
 
@@ -21,6 +23,5 @@ ProductSchema.plugin(mongoosastic, {
     }
   ]
 })
-console.log('process.env.bonsai', process.env.BONSAI_URL)
 
-module.exports = mongoose.model('Product', ProductSchema)
+module.exports = mongoose.model('product', ProductSchema)
